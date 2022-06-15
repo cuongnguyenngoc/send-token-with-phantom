97fb085c9d28ef81a52099c2c5c620b8e05a6ba2
justin <ngoccuongbka94@gmail.com>
2022-06-15 14:42:23 +0700

    send txn transaction to custodial wallet bridge endpoint to transfer offchain item to user inv

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { WalletAdapterNetwork, WalletNotConnectedError } from '@solana/wallet-ad
 import { ConnectionProvider, WalletProvider, useWallet } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import * as web3 from '@solana/web3.js';
- 
+import { toast,ToastContainer } from "react-toastify";
+
 import {getOrCreateAssociatedTokenAccount,createTransferInstruction,TOKEN_PROGRAM_ID} from "@solana/spl-token"; // @ts-nocheck
 
 import '../src/css/bootstrap.css'
@@ -22,15 +23,15 @@ import { clusterApiUrl, Transaction, LAMPORTS_PER_SOL } from '@solana/web3.js';
 import { FC, ReactNode, useMemo, useCallback, useState } from 'react';
 
 import { Connection } from '@metaplex/js';
+import axios from 'axios';
 
 require('./App.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
+require('react-toastify/dist/ReactToastify.css');
 
 const theWallet = "8QEz51cud4EUjyd2tY3BfHvxWLPaXrVfdmjJqz9Hb432"
 
 const App: FC = () => {
-
-
   return (
     <Context>
       <Content />
@@ -76,8 +77,13 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
   );
 };
 
-const Content: FC = () => {
+const Content: FC =  () => {
   let [igsNum, setIgsNum] = useState("1");
+  let [sIgsInv, setSIgsInv] = useState(0);
+  
+  axios.get("http://10.5.1.51:34572/inventory/540004719935094795")
+  .then(res=> setSIgsInv(res.data.sIgsAmount));
+   
 
   // const { connection } = useConnection();
   const connection = new Connection(clusterApiUrl(WalletAdapterNetwork.Mainnet))
@@ -93,6 +99,21 @@ const Content: FC = () => {
       console.log(bal / LAMPORTS_PER_SOL);
     });
     console.log("publicKey",publicKey);
+
+    // try {
+    //   await axios.post("http://10.5.1.51:34572/bridge/in", {
+    //     transactionSignature: "4Hfm5ob7bpNZEkev1gWG6SX53XpsSVufqFEP2sYN83AZrwaTrBgqhoAP3b7SsHqbhCZGYtEUMJXGRPW5D1nrKYH9"
+    //   });
+
+    //   await axios.get("http://10.5.1.51:34572/inventory/540004719935094795")
+    //   .then(res=> setSIgsInv(res.data.sIgsAmount));
+
+    //   toast.success(`${igsNum} sIGS has been exchanged by ${igsNum} IGS`);
+    // } catch(err) {
+    //   console.log('err', err?.response?.data?.err?.msg);
+    //   toast.error(err?.response?.data?.err?.msg)
+    // }
+    // return;
 
     // Get the token account of the fromWallet address, and if it does not exist, create it
     const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
@@ -113,6 +134,7 @@ const Content: FC = () => {
     );
     console.log("toTokenAccount",toTokenAccount.address)
 
+    console.log("igsNum",igsNum);
     const transaction = new Transaction().add(
       createTransferInstruction(
         fromTokenAccount.address, // source
@@ -133,17 +155,36 @@ const Content: FC = () => {
 
     const signature = await connection.sendRawTransaction(signed.serialize())
     console.log('transaction sent', signature);
+    toast.success('transaction sent', signature);
+
     await connection.confirmTransaction(signature, 'finalized');
-    console.log("confirm transaction");
+    console.log('transaction sent', signature);
+    toast.success("confirmed transaction");
+
+    try {
+      await axios.post("http://10.5.1.51:34572/bridge/in", {
+        transactionSignature: signature
+      });
+
+      await axios.get("http://10.5.1.51:34572/inventory/540004719935094795")
+      .then(res=> setSIgsInv(res.data.sIgsAmount));
+
+      toast.success(`${igsNum} sIGS has been exchanged by ${igsNum} IGS`);
+    } catch(err) {
+      console.log('err', err?.response?.data?.err?.msg);
+      toast.error(err?.response?.data?.err?.msg)
+    }
   }, [publicKey, sendTransaction, connection]);
 
   return (
     <div className="App">
       <div className="navbar">
         <div className="navbar-inner ">
-          <a id="title" className="brand" href="#">Send IGS to 8QEz51cud4EUjyd2tY3BfHvxWLPaXrVfdmjJqz9Hb432</a>
+          <a id="title" className="brand" href="#">Send IGS to 8QEz51cud4EUjyd2tY3BfHvxWLPaXrVfdmjJqz9Hb432 to buy sIGS</a>
           <ul className="nav">
           </ul>
+          <h4>User current sIGS: {sIgsInv}</h4>
+          <ul className="nav"></ul>
           <ul className="nav pull-right">
             <li className="divider-vertical"></li>
             <li><WalletMultiButton /></li>
@@ -153,9 +194,10 @@ const Content: FC = () => {
       </div>
       <input value={igsNum} type="number" onChange={(e) => setIgsNum(e.target.value)}></input>
       <br></br>
-      <button className='btn' onClick={onClick}>Send IGS </button>
-
-
+      <button className='btn' onClick={onClick}>
+        Buy sIGS by IGS
+        </button>
+        <ToastContainer />
     </div>
   );
 };
